36e193991f15efc2398da9209f3ffdb70a0ce20c
1435378599 <71108633+1435378599@users.noreply.github.com>
2020-09-11 11:07:31 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate: ");
         Log.e(TAG, "onCreate: ",);
         Log.w(TAG, "onCreate: ");
+        System.out.println("dddddd");
 
     }
 }
